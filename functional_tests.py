38b9df85a244b143e16e44460caf792a36508df1
Daniel Acaz <daniel_acaz@hotmail.com>
2019-04-20 00:26:18 -0300

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -17,14 +19,27 @@ class NewVisitorTest(unittest.TestCase):
 
 		#She sees that the title of page and your header say 'to-do'
 		self.assertIn('To-Do', self.browser.title)
-		self.fail('Finish the test!')
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
 
 		#She is invited to insert one taks item immediately	
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+		)
 
 		#She types "Buy peacock feathers" inside of a box of table (your hooby is make fly fishing lures)
-
+		inputbox.send_keys('Buy peacock feathers')
 		#When she enter key, the page is updated. And now, this page show "1: Buy peacock feathers" like an item inside a task list
+		inputbox.send_keys(Keys.ENTER)
+		time.sleep(1)
 
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_element_by_tag_name('tr')
+		self.assertTrue(
+			any(row.text == '1: Buy peacock feathers' for row in rows)
+		)
 		#There is still a box inveting her to add other item. She inserts "Use peacock feathers to make a fly". She is methodic 
 
 		#The page is updated again and now show the two itens in your list
@@ -34,6 +49,7 @@ class NewVisitorTest(unittest.TestCase):
 		#She access this URL and your list still continues there.
 
 		#So, she sleeps now.
+		self.fail('Finish the test!')
 
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
