1c692a5e7f1dc6adeee3d429eedde1b96f84d9c3
Daniel <daniel.acaz@becommerce.com.br>
2019-05-02 16:29:37 -0300

    Front page HTML now generate from a template

--- functional_tests.py
+++ functional_tests.py
@@ -38,7 +38,7 @@ class NewVisitorTest(unittest.TestCase):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_element_by_tag_name('tr')
 		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows)
+			any(row.text == '1: Buy peacock feathers' for row in rows), "New to-do item did not appear in table"
 		)
 		#There is still a box inveting her to add other item. She inserts "Use peacock feathers to make a fly". She is methodic 
 
